c9ceefaccddc778397fa20153cddd9e93d73a59b
MirkoDL <github@mdl.simplelogin.com>
2023-01-09 23:14:44 +0100

    added basic elements

---
+++ script/index.js
@@ -0,0 +1,6 @@
+
+//when a button in SELECTOR DIV is clicked show a child button and add a textArea below for the message input
+document.getElementById('benvenuto-btn').addEventListener('click', e =>{
+    document.getElementById("benvenuto-btn").classList.toggle('off');
+    
+}); 
